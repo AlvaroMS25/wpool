c6acd58a19d443ae280aec459aca236745c4944b
AlvaroMS25 <alvaro2ms5@gmail.com>
2022-10-11 10:00:16 +0200

    docs

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,14 @@
 use std::any::Any;
 
+/// The error that can be returned after spawning a task.
+/// This will be only seen when the provided task panics or the pool is stopped before the task
+/// could be executed.
 #[derive(Debug)]
 pub enum Error {
+    /// The task has panicked and the error is returned
     Panicked(Box<dyn Any + Send + 'static>),
+    /// The task has been aborted, this is seen when the pool was stopped and the task didn't
+    /// get to be executed before stopping.
     Aborted
 }
 
